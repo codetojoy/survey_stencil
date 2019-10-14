a5ac3d6a8f7bae41ef7c2a0dc30af853add80bac
Michael Easter <codetojoy@gmail.com>
2019-10-14 16:37:16 -0300

    comment for hard-coded URL

--- component-based/my-component/src/services/question-controller.ts
+++ component-based/my-component/src/services/question-controller.ts
@@ -13,6 +13,7 @@ export class QuestionController {
           // console.log('TRACER controller then no-op');
           // no-op
         } else {
+          // ******* CHANGE THIS for your machine
           let response = await fetch("http://localhost:5151/api-server/rest/v1/surveys/302");
           let json = await response.json();
 
